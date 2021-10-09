33b2300f98bf882e05c3796f6106dcb03261f06c
Wyjson <wyjson@qq.com>
2021-10-09 10:08:50 +0800

    fix

--- app/src/main/java/com/wyjson/actionbardemo/view/IconTextView.java
+++ app/src/main/java/com/wyjson/actionbardemo/view/IconTextView.java
@@ -24,8 +24,7 @@ public class IconTextView extends androidx.appcompat.widget.AppCompatTextView {
 
     public IconTextView(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        init(context
-        );
+        init(context);
     }
 
     public IconTextView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
